dab83d011e760ab8752684f78fdba64bd29959bd
Sayali Borse <borse.s@husky.neu.edu>
2020-02-12 16:11:41 -0500

    Create signprac.php

---
+++ app_store/signprac.php
@@ -0,0 +1,63 @@
+<?php
+$first_name = $_POST['FName'];
+$mi = $_POST['mi'];
+$last_name = $_POST['LName'];
+$contact = $_POST['Contact'];
+$email = $_POST['Email'];
+$address = $_POST['Address'];
+$city = $_POST['City'];
+$zip_code = $_POST['Zip_code'];
+$state = $_POST['State'];
+$username = $_POST['Username'];
+$password = $_POST['password'];
+$login_type = $_POST['gridRadios'];
+
+$con = mysqli_connect("localhost","root","","appstore");
+if (!$con)
+  {
+	die('Could not connect: ' . mysql_error());
+  }
+
+$get=mysqli_query($con,"SELECT MAX(User_ID) FROM user"); 
+$got = mysqli_fetch_array($get); 
+$next_id = $got['MAX(User_ID)'] + 1;  
+echo $next_id;   
+$sql = "
+   CREATE TRIGGER trigger1 AFTER INSERT ON user
+      FOR EACH ROW BEGIN
+   		INSERT INTO user 
+         VALUES (
+            '$next_id','$first_name','$mi','$last_name','$contact','$email','$address','$city','$zip_code','$state';
+
+            );
+      END;
+   ";
+
+$res = mysqli_multi_query($con,$sql);
+echo $res;
+
+$sql_login = "INSERT INTO login VALUES ('$username','$password','$login_type')";
+$result1 = mysqli_query($con,$sql_login);
+
+
+if(mysqli_error($con)){
+		printf("error: %s\n", mysqli_error($con));
+}
+
+if($res == TRUE){
+	
+	echo "User Trigger Fired!!!";
+}
+
+
+
+if($result1 == TRUE){
+	
+	echo "User Login Details Added!!!";
+}
+
+
+
+
+mysqli_close($con);
+?>
